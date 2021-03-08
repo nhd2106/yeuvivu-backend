e50765c6606f62b94414669f5d03b8dc077802c2
Duoc Ngo <nhd.ars@gmail.com>
2021-03-08 22:04:12 +0700

    yeuvivu-backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a8f3b564ffbb06be667c00aca8aa32af'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2e9ece6-210b-4aa2-9200-594ee808a9f9'
+};
